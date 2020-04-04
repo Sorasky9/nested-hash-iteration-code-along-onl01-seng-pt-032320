f73d95b2bc620bfdfc8232700f1dfb1e87b2856c
Malcom Fetus <sora.vista@gmail.com>
2020-04-04 21:32:25 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
 contacts.each do |person, details| #Here we are iterating the key of NAMES (person) in the contacts hash (Jon Snow and Freddy Mercury) and the keys of DETAILS (detials) of the names in the contacts hash.
 if person == "Freddy Mercury" # We are saying if any person in the contacts hash is named Freddy Mercury
   details.each do |attribute, data| # We should iterate over details; which are 'subhashes' of information related to Freddy Mercury. attribute has keys of names, email, favorite ice cream flavors and the data are the values of the keys of attribute we will iterate as well.
-    binding.pry
+    #binding.pry
 
 if attribute == "favorite_ice_cream_flavors"
 data.delete_if {|ice_cream| ice_cream == "strawberry"}
